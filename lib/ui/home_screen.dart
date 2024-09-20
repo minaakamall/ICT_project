df0ffb6d4dc55ff0553de2d2a2d53bdc0176e0b6
minaakamall <minakamal696@yahoo.com>
2024-09-20 16:37:21 +0300

    ICT

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ictproject/ui/display.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Display(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: const DecorationImage(
+                        image: AssetImage(
+                          'assets/image/cat.webp',
+                        ),
+                        fit: BoxFit.cover),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 40,
+                      color: Colors.black.withOpacity(0.5),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            'click',
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Icon(
+                            Icons.access_alarm_rounded,
+                            size: 40,
+                            color: Colors.blue,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.arrow_back_ios_rounded,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.amber,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 300,
+                child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 75, 140, 194),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: ListTile(
+                          leading: Image.asset('assets/image/dog.jpg'),
+                          title: Text('Cat'),
+                          trailing: Icon(
+                            Icons.delete_forever,
+                            color: Colors.black,
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                          height: 20,
+                        ),
+                    itemCount: 3),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
